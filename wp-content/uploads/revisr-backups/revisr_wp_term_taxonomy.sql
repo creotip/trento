f201f0adad4831b819520402d181bc46ee7c8ec9
adminus <creotip@gmail.com>
2016-02-12 10:25:44 -0500

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_term_taxonomy.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_term_taxonomy.sql
@@ -22,12 +22,12 @@ CREATE TABLE `wp_term_taxonomy` (
   PRIMARY KEY (`term_taxonomy_id`),
   UNIQUE KEY `term_id_taxonomy` (`term_id`,`taxonomy`),
   KEY `taxonomy` (`taxonomy`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_term_taxonomy` WRITE;
 /*!40000 ALTER TABLE `wp_term_taxonomy` DISABLE KEYS */;
-INSERT INTO `wp_term_taxonomy` VALUES (1,1,'category','',0,1),(2,2,'nav_menu','',0,11),(3,3,'category','',0,4),(4,4,'post_tag','',0,0),(5,5,'post_tag','',0,0),(6,6,'post_tag','',0,0),(7,7,'post_tag','',0,0),(8,8,'post_tag','',0,0),(9,9,'post_tag','',0,0),(10,10,'post_tag','',0,0),(11,11,'custom_cat','',0,1),(12,12,'custom_tag','',0,1),(13,13,'custom_tag','',0,1),(14,14,'custom_tag','',0,1),(15,15,'nav_menu','',0,14),(16,16,'product_type','',0,21),(17,17,'product_type','',0,0),(18,18,'product_type','',0,2),(19,19,'product_type','',0,0),(20,20,'pa_color','',0,2),(21,21,'pa_color','',0,1),(22,22,'pa_color','',0,1),(23,23,'product_cat','',0,12),(24,24,'product_cat','',23,6),(25,25,'product_cat','',0,6),(26,26,'product_cat','',0,5),(27,27,'product_cat','',25,2),(28,28,'product_cat','',23,6),(29,29,'product_cat','',25,4);
+INSERT INTO `wp_term_taxonomy` VALUES (1,1,'category','',0,1),(2,2,'nav_menu','',0,11),(3,3,'category','',0,4),(4,4,'post_tag','',0,0),(5,5,'post_tag','',0,0),(6,6,'post_tag','',0,0),(7,7,'post_tag','',0,0),(8,8,'post_tag','',0,0),(9,9,'post_tag','',0,0),(10,10,'post_tag','',0,0),(11,11,'custom_cat','',0,1),(12,12,'custom_tag','',0,1),(13,13,'custom_tag','',0,1),(14,14,'custom_tag','',0,1),(15,15,'nav_menu','',0,14),(16,16,'product_type','',0,21),(17,17,'product_type','',0,0),(18,18,'product_type','',0,3),(19,19,'product_type','',0,0),(20,20,'pa_color','',0,4),(21,21,'pa_color','',0,3),(22,22,'pa_color','',0,3),(23,23,'product_cat','',0,13),(24,24,'product_cat','',23,6),(25,25,'product_cat','',0,6),(26,26,'product_cat','',0,5),(27,27,'product_cat','',25,2),(28,28,'product_cat','',23,7),(29,29,'product_cat','',25,4),(30,30,'pa_color','',0,1);
 /*!40000 ALTER TABLE `wp_term_taxonomy` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
--- wp-content/uploads/revisr-backups/revisr_wp_woocommerce_termmeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_woocommerce_termmeta.sql
@@ -20,12 +20,12 @@ CREATE TABLE `wp_woocommerce_termmeta` (
   PRIMARY KEY (`meta_id`),
   KEY `woocommerce_term_id` (`woocommerce_term_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_woocommerce_termmeta` WRITE;
 /*!40000 ALTER TABLE `wp_woocommerce_termmeta` DISABLE KEYS */;
-INSERT INTO `wp_woocommerce_termmeta` VALUES (1,23,'order','0'),(2,24,'order','0'),(3,25,'order','0'),(4,26,'order','0'),(5,27,'order','0'),(6,28,'order','0'),(7,29,'order','0'),(8,23,'product_count_product_cat','12'),(9,28,'product_count_product_cat','6'),(10,24,'product_count_product_cat','6'),(11,26,'product_count_product_cat','5'),(12,29,'product_count_product_cat','4'),(13,25,'product_count_product_cat','6'),(14,27,'product_count_product_cat','2'),(15,20,'product_ids','a:2:{i:0;s:3:\"163\";i:1;s:3:\"167\";}'),(16,21,'product_ids','a:1:{i:0;s:3:\"167\";}'),(17,22,'product_ids','a:1:{i:0;s:3:\"163\";}'),(18,23,'display_type',''),(19,23,'thumbnail_id','194');
+INSERT INTO `wp_woocommerce_termmeta` VALUES (1,23,'order','0'),(2,24,'order','0'),(3,25,'order','0'),(4,26,'order','0'),(5,27,'order','0'),(6,28,'order','0'),(7,29,'order','0'),(8,23,'product_count_product_cat','13'),(9,28,'product_count_product_cat','7'),(10,24,'product_count_product_cat','6'),(11,26,'product_count_product_cat','5'),(12,29,'product_count_product_cat','4'),(13,25,'product_count_product_cat','6'),(14,27,'product_count_product_cat','2'),(18,23,'display_type',''),(19,23,'thumbnail_id','194'),(20,30,'order_pa_color','0');
 /*!40000 ALTER TABLE `wp_woocommerce_termmeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
